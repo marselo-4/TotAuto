20d26460354e8d0f67306197d93d4b6e9dd44713
marselo.exe <78843770+marselo-4@users.noreply.github.com>
2024-03-14 13:08:19 +0100

    Contacto+PHP+JS+Llantas

--- mail.php
+++ mail.php
@@ -3,8 +3,8 @@
   $name = $_POST['name'];
   if(isset( $_POST['email']))
   $email = $_POST['email'];
-  if(isset( $_POST['tlf']))
-  $tlf = $_POST['tlf'];
+  if(isset( $_POST['telefono']))
+  $telefono = $_POST['telefono'];
   if(isset( $_POST['empresa']))
   $empresa = $_POST['empresa'];
   if(isset( $_POST['message']))
@@ -12,9 +12,17 @@
   if(isset( $_POST['subject']))
   $subject = $_POST['subject'];
 
-  $content="De: $name \n Email: $email \n Telefono: $tlf \n Empresa: $empresa \n Mensaje: $message";
+  $content="De: $name \n Email: $email \n Telefono: $telefono \n Empresa: $empresa \n Mensaje: $message";
   $recipient = "info@totauto";
   $mailheader = "De: $email \r\n";
   mail($recipient, $subject, $content, $mailheader) or die("Error!");
   echo "Email enviado!";
+  <script>
+  function enviado() {
+    Swal.fire({
+        title: "Mensaje Enviado!",
+        icon: "success"
+      });
+}
+  </script>
 ?>
--- script.js
+++ script.js
@@ -1,76 +1,34 @@
-function validateForm() {
-    var name =  document.getElementById('name').value;
-    if (name == "") {
-        document.querySelector('.status').innerHTML = "Nombre no puede estar vacio";
-        return false;
+
+
+function enviandoForm() {
+  let timerInterval;
+  Swal.fire({
+    title: "Enviando Mensaje!",
+    html: "Nuestro equipo te contactará en breves.",
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const timer = Swal.getPopup().querySelector("b");
+      timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
     }
-    var email =  document.getElementById('email').value;
-    if (email == "") {
-        document.querySelector('.status').innerHTML = "Email no puede estar vacio";
-        return false;
-    } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
-            return false;
-        }
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("Ha acabado el temporizador SwAl");
     }
-    var tlf =  document.getElementById('tlf').value;
-    if (tlf == "") {
-        document.querySelector('.status').innerHTML = "Telefono no puede estar vacio";
-        return false;
-    }
-    var subject =  document.getElementById('subject').value;
-    if (subject == "") {
-        document.querySelector('.status').innerHTML = "Asunto no puede estar vacio";
-        return false;
-    }
-    var message =  document.getElementById('message').value;
-    if (message == "") {
-        document.querySelector('.status').innerHTML = "Mensaje no puede estar vacio";
-        return false;
-    }
-    document.querySelector('.status').innerHTML = "Enviando...";
-
-  }
-
-    document.getElementById('status').innerHTML = "enviando...";
-    formData = {
-      'name': $('input[name=name]').val(),
-      'email': $('input[name=email]').val(),
-      'tlf' : $('input[name=tlf]').val(),
-      'empresa': $('input[name=empresa]').val(),
-      'subject': $('input[name=subject]').val(),
-      'message': $('textarea[name=message]').val()
-    };
-
-
-    $.ajax({
-      url: "mail.php",
-      type: "POST",
-      data: formData,
-      success: function (data, textStatus, jqXHR) {
-
-        $('#status').text(data.message);
-        if (data.code) //If mail was sent successfully, reset the form.
-          $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $('#status').text(jqXHR);
-      }
-    });
-
-function submit() {
-    Swal.fire({
-        title: "Mensaje Enviado!",
-        icon: "success"
-      });
+  });
 }
 
-function error() {
+function errorform() {
     Swal.fire({
         icon: "error",
-        title: "Oops...",
-        text: "Algo ha salido mal!",
+        title: "Ha ocurrido un problema",
+        text: "Revisa que hayas completado los campos necesarios!",
       });
 }
